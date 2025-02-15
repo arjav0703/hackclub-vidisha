dc06bd83e1259cff7a967caaaa8c649a492043a3
Arjav Jain <arjavjain0703@gmail.com>
2025-02-15 05:04:26 +0000

    created a blank page

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "5a86dd00b7867dd2"; }
+/******/ 		__webpack_require__.h = function() { return "f929a41ae8cf0e1d"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
