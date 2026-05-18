d48ce210194c890af6201ae159afd58a56ac52e2
Arjav Jain <arjavjain0703@gmail.com>
2026-05-18 16:45:43 +0530

    refactor hero section

---
+++ components/hero.js
@@ -0,0 +1,68 @@
+import styled from "@emotion/styled";
+import { Card, Container, Flex, Text } from "theme-ui";
+
+export default function Hero() {
+    return (
+    <>
+    {/* <Container sx={{ my: 5 }} variant="wide">
+      <Flex sx={{ alignItems: 'center', flexWrap: 'wrap' }}>
+        <Text variant="title" color="orange">
+          Coding is a <Super>superpower</Super>
+        </Text>
+        <Container variant="copy">
+          <Card
+            variant="interactive"
+            sx={{
+              my: 3,
+              backgroundImage: t => t.util.gx('#f89b29', '#ff0f7b'),
+              color: 'white'
+            }}
+          >
+            <Text variant="subtitle" color="darker">
+              Learning to code is uniquely like gaining a superpower: it
+              converts you from a consumer to a creator. Suddenly, computers
+              become a tool for creating.
+            </Text>
+          </Card>
+        </Container>
+      </Flex>
+    </Container> */}
+
+    <Container sx={{ my: 5 }} variant="wide">
+      <Flex sx={{ alignItems: 'center', flexWrap: 'wrap' }}>
+        <Text variant="title" color="green">
+          Start Building. <Super>Now</Super>{' '}
+        </Text>
+        <Container variant="copy">
+          <Card
+            variant="interactive"
+            sx={{
+              my: 3,
+              backgroundImage: t => t.util.gx('#57ebde', '#aefb2a'),
+              color: 'white'
+            }}
+          >
+            <Text variant="subtitle" color="black">
+              Most coding classes teach you programming concepts instead of how
+              to write real code—it’s like trying to learn carpentry without any
+              wood. So at Hack Club, you learn to code entirely through building
+              things. You start with no experience and build and ship a project
+              every meeting.
+            </Text>
+          </Card>
+        </Container>
+      </Flex>
+    </Container>
+    </>
+    )
+}
+
+const Super = styled(Text)`
+  background-color: rgb(33, 165, 40);
+  clip-path: polygon(4% 0%, 100% 0%, 96% 100%, 0% 100%);
+  color: rgb(255, 255, 255);
+  display: inline-block;
+  padding-bottom: 12px;
+  padding-left: 18px;
+  padding-right: 18px;
+`
--- pages/index.js
+++ pages/index.js
@@ -33,6 +33,7 @@ import Flag from '../components/flag'
 import React from 'react'
 import Team from '../components/team'
 import Faq from '../components/faq'
+import Hero from '../components/hero'
 // Next.js allows fast development of static sites with server-side rendering
 
 // Fast development my ass!
@@ -54,15 +55,6 @@ const Row = styled(Container)`
 
 Row.defaultProps = { sx: { px: 3, py: [3, 4], color: 'black' } }
 
-const Super = styled(Text)`
-  background-color: rgb(228, 115, 45);
-  clip-path: polygon(4% 0%, 100% 0%, 96% 100%, 0% 100%);
-  color: rgb(255, 255, 255);
-  display: inline-block;
-  padding-bottom: 12px;
-  padding-left: 18px;
-  padding-right: 18px;
-`
 
 const HomePage = () => (
   <>
@@ -131,55 +123,7 @@ const HomePage = () => (
     
     <UpcomingEvents/>
      
-    <Container sx={{ my: 5 }} variant="wide">
-      <Flex sx={{ alignItems: 'center', flexWrap: 'wrap' }}>
-        <Text variant="title" color="orange">
-          Coding is a <Super>superpower</Super>
-        </Text>
-        <Container variant="copy">
-          <Card
-            variant="interactive"
-            sx={{
-              my: 3,
-              backgroundImage: t => t.util.gx('#f89b29', '#ff0f7b'),
-              color: 'white'
-            }}
-          >
-            <Text variant="subtitle" color="darker">
-              Learning to code is uniquely like gaining a superpower: it
-              converts you from a consumer to a creator. Suddenly, computers
-              become a tool for creating.
-            </Text>
-          </Card>
-        </Container>
-      </Flex>
-    </Container>
-
-    <Container sx={{ my: 5 }} variant="wide">
-      <Flex sx={{ alignItems: 'center', flexWrap: 'wrap' }}>
-        <Text variant="title" color="green">
-          Start Building. Now.{' '}
-        </Text>
-        <Container variant="copy">
-          <Card
-            variant="interactive"
-            sx={{
-              my: 3,
-              backgroundImage: t => t.util.gx('#57ebde', '#aefb2a'),
-              color: 'white'
-            }}
-          >
-            <Text variant="subtitle" color="black">
-              Most coding classes teach you programming concepts instead of how
-              to write real code—it’s like trying to learn carpentry without any
-              wood. So at Hack Club, you learn to code entirely through building
-              things. You start with no experience and build and ship a project
-              every meeting.
-            </Text>
-          </Card>
-        </Container>
-      </Flex>
-    </Container>
+    <Hero/>
 
     <Faq/>
 
